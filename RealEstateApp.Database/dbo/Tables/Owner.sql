3bd1ce422f0709a1affe9e96970625c2408875de
WillyTchana <willyjoeltchana@gmail.com>
2021-04-15 10:59:33 +0100

    Add project files.

---
+++ RealEstateApp.Database/dbo/Tables/Owner.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Owner] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (100) NOT NULL,
+    [CreatedDate] DATETIME       NOT NULL,
+    CONSTRAINT [PK_Owner] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [IX_Owner] UNIQUE NONCLUSTERED ([Name] ASC)
+);
+
---
+++ RealEstateApp.Database/dbo/Tables/Property.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Property] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Reference]   NVARCHAR (5)   NOT NULL,
+    [Name]        NVARCHAR (100) NOT NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [Price]       FLOAT (53)     NOT NULL,
+    [CreatedDate] DATETIME       NOT NULL,
+    [OwnerId]     INT            NOT NULL,
+    CONSTRAINT [PK_Property] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Property_Owner] FOREIGN KEY ([OwnerId]) REFERENCES [dbo].[Owner] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [IX_Property] UNIQUE NONCLUSTERED ([Reference] ASC)
+);
+
